0be56e77441508224665cb95d029d968e36cba13
Murali <ns4aofficial@gmail.com>
2025-11-21 02:24:42 +0530

    fixed admin: added deliveryaddress import and registration

--- users/admin.py
+++ users/admin.py
@@ -2,48 +2,42 @@ from django.contrib import admin
 from .models import User, DeliveryAddress
 
 
-# ------------------------------
-# USER ADMIN
-# ------------------------------
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "username", "email", "phone",
-        "role", "captain_id", "vehicle_number", "city",
+        "id",
+        "username",
+        "email",
+        "phone",
+        "role",
+        "captain_id",
+        "vehicle_number",
+        "city",
         "notifications_enabled",
     )
-
     list_filter = ("role", "city", "notifications_enabled")
-    search_fields = ("username", "email", "phone", "captain_id", "vehicle_number")
-
-    fieldsets = (
-        ("User Info", {
-            "fields": ("username", "email", "phone", "password")
-        }),
-        ("Roles", {
-            "fields": ("role",)
-        }),
-        ("Captain Details", {
-            "fields": ("captain_id", "vehicle_number", "city"),
-            "classes": ("collapse",)
-        }),
-        ("Settings", {
-            "fields": ("notifications_enabled",)
-        }),
+    search_fields = (
+        "username",
+        "email",
+        "phone",
+        "captain_id",
+        "vehicle_number",
+        "city",
     )
 
-    readonly_fields = ("password",)
 
-
-# ------------------------------
-# DELIVERY ADDRESS ADMIN
-# ------------------------------
 @admin.register(DeliveryAddress)
 class DeliveryAddressAdmin(admin.ModelAdmin):
     list_display = (
-        "id", "user", "full_name", "city",
-        "pincode", "phone", "default", "created_at"
+        "id",
+        "user",
+        "full_name",
+        "phone",
+        "pincode",
+        "city",
+        "state",
+        "default",
+        "created_at",
     )
-
-    list_filter = ("city", "default")
-    search_fields = ("full_name", "city", "pincode", "phone", "house", "street")
+    list_filter = ("city", "state", "default")
+    search_fields = ("full_name", "phone", "city", "state", "pincode")
