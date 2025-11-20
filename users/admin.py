618be8a79a8a7b4619aefe9dd7eabd3591012f1e
Murali <ns4aofficial@gmail.com>
2025-11-21 02:52:18 +0530

    fixed merge conflict in admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,56 +1,15 @@
 from django.contrib import admin
-from .models import User   # REMOVE DeliveryAddress import
-
-<<<<<<< HEAD
+from .models import User
+from .models import DeliveryAddress
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "username",
-        "email",
-        "phone",
-        "role",
-        "captain_id",
-        "vehicle_number",
-        "city",
-        "notifications_enabled",
-    )
-    list_filter = ("role", "city", "notifications_enabled")
-    search_fields = (
-        "username",
-        "email",
-        "phone",
-        "captain_id",
-        "vehicle_number",
-        "city",
-    )
-
+    list_display = ('id', 'username', 'email', 'phone', 'role', 'captain_id', 'vehicle_number', 'city')
+    list_filter = ('role', 'city')
+    search_fields = ('username', 'email', 'phone', 'captain_id')
 
 @admin.register(DeliveryAddress)
 class DeliveryAddressAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "user",
-        "full_name",
-        "phone",
-        "pincode",
-        "city",
-        "state",
-        "default",
-        "created_at",
-    )
-    list_filter = ("city", "state", "default")
-    search_fields = ("full_name", "phone", "city", "state", "pincode")
-=======
-# Register Custom User
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-        'id', 'email', 'username', 'role', 'phone',
-        'captain_id', 'vehicle_number', 'city',
-        'is_staff'
-    )
-    search_fields = ('email', 'username', 'phone', 'role', 'city')
-    list_filter = ('role', 'city', 'is_staff')
->>>>>>> ad0f9a3 (fixed admin import:removed delivewryaddress import)
+    list_display = ('id', 'user', 'full_name', 'phone', 'city', 'state', 'pincode', 'default')
+    search_fields = ('full_name', 'phone', 'city')
+    list_filter = ('city', 'state', 'default')
